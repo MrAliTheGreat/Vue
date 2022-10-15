3ae4627ea9d101da494f210c30103cd662dfcc1a
Ali Bahari <alibahari007@gmail.com>
2022-10-15 16:00:52 +0330

    Add a basic new component to App

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
         Hello World!
+        <SearchBar></SearchBar>
     </div>
 </template>
 
 <script>
+import SearchBar from "./components/SearchBar.vue"
+
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        SearchBar
+    }
 }
 </script>
 
 <style>
 </style>
+
+
+
+<!-- We can only have one root element in template like returning a View in react native!  -->
+<!-- We must have only one element in template -->
