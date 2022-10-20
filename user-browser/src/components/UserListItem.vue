db56b66e43b8dad61bfa18bc90fd18d3cc3b3597
Ali Bahari <alibahari007@gmail.com>
2022-10-20 17:26:38 +0330

    Add user photo to user list

--- user-browser/src/components/UserListItem.vue
+++ user-browser/src/components/UserListItem.vue
@@ -1,15 +1,29 @@
 <template>
     <li class="list-group-item">
-        {{ user.name.first + " " + user.name.last }} - {{ user.email }} - {{ user.cell }}
+        <img :src="thumbnailURL" />
+        {{ userFullName }} - {{ user.email }} - {{ user.cell }}
     </li>
 </template>
 
 <script>
 export default {
     name: "UserListItem",
-    props: ["user"]
+    props: ["user"],
+    computed: {
+        thumbnailURL() {
+            return this.user.picture.thumbnail
+        },
+        userFullName() {
+            return this.user.name.first + " " + this.user.name.last
+        }
+    }
 }
 </script>
 
 <style>
 </style>
+
+
+<!-- For showing and image via URL we have to pass src as a prop! -->
+<!-- We can also use computed to work with props! -->
+<!-- Computed is used here to try moving logic from template to script and it's just a better way of coding! -->
