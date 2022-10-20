f9e60c44c73d1792341799d921d49122a565a29f
Ali Bahari <alibahari007@gmail.com>
2022-10-20 20:52:17 +0330

    Add semantic-ui to the project

--- images/src/App.vue
+++ images/src/App.vue
@@ -1,14 +1,24 @@
 <template>
-    <div>
-        Main Page!
-    </div>
+    <AppHeader></AppHeader>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue"
+
 export default {
-    name: "App"
+    name: "App",
+    components: {
+        AppHeader
+    }
 }
 </script>
 
 <style>
 </style>
+
+
+
+<!-- For handling css we are going to use semantic-ui -->
+<!-- Installing semantic-ui is going to happen through cdnjs.com which is a content delivery network -->
+<!-- We want to search for semantic-ui and get the link tag for semantic.min.css -->
+<!-- We then add it to head of index.html then we have it installed! -->
